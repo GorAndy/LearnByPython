d50fc5f07a4df9f9c3f326601d2628f9519e0b48
GorAndy <AndreyG1@MacBook.local>
2017-03-11 20:58:08 +0300

    Домашнее задание 2.1

--- HomeWork/HW_2_1.py
+++ HomeWork/HW_2_1.py
@@ -1,21 +1,23 @@
 
 
 def add_to(recipe):
-    book = {}
+    dish_recipe = {}
     name_dish = recipe[0]
-    book[name_dish] = []
-    for i in range(int(recipe[1])):
-        ingrid = recipe[i+2].split('|')
-        book[name_dish].append({'ingridient_name': ingrid[0], 'quantity': int(ingrid[1]), 'measure': ingrid[2]})
-    return book
+    dish_recipe[name_dish] = []
+    for ingridient in recipe[2::]:
+        ingridient = ingridient.split('|')
+        dish_recipe[name_dish].append({'ingridient_name': ingridient[0].strip(),\
+                                       'quantity': int(ingridient[1]), 'measure': ingridient[2].strip()})
+    return dish_recipe
 
 
 def get_cook_book():
     recipe = []
+    cook_book = {}
     with open('2_1_cook_book.txt', 'r') as cook:
         for line in cook:
             line = line.strip()
-            if line == '':
+            if not line:
                 cook_book.update(add_to(recipe))
                 recipe = []
             else:
@@ -23,7 +25,9 @@ def get_cook_book():
         cook_book.update(add_to(recipe))
     return cook_book
 
+
 def get_shop_list_by_dishes(dishes, person_count):
+    cook_book = get_cook_book()
     shop_list = {}
     for dish in dishes:
         for ingridient in cook_book[dish]:
@@ -38,34 +42,13 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 def print_shop_list(shop_list):
     for shop_list_item in shop_list.values():
-        # print(shop_list)
         print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
 
 
 def create_shop_list():
-    cook_book = get_cook_book()
     person_count = int(input('Введите количество едоков: '))
     dishes = input('Введите блюда в расчете на одного человека (через запятую):').lower().split(', ')
     shop_list = get_shop_list_by_dishes(dishes, person_count)
     print_shop_list(shop_list)
 
-
-# cook_book = {
-#     'яичница': [
-#         {'ingridient_name': 'eggs', 'quantity': 2, 'measure': 'шт'},
-#         {'ingridient_name': 'tomat', 'quantity': 100, 'measure': 'гр'}
-#     ],
-#     "стейк": [
-#         {'ingridient_name': 'мясо', 'quantity': 300, 'measure': 'гр'},
-#         {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр'},
-#         {'ingridient_name': 'масло', 'quantity': 10, 'measure': 'гр'}
-#     ],
-#     "салат": [
-#         {'ingridient_name': 'tomat', 'quantity': 100, 'measure': 'гр'},
-#         {'ingridient_name': 'огрурцы', 'quantity': 100, 'measure': 'гр'},
-#         {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'гр'},
-#         {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт'}
-#     ]
-# }
-cook_book = {}
 create_shop_list()
